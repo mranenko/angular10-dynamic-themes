ab5be117784516c057bc661e59c3d6b1747912cb
Marina Ranenko <mranenko@gmail.com>
2020-10-02 13:48:34 -0600

    Added local storage service

---
+++ src/app/services/storage.service.ts
@@ -0,0 +1,38 @@
+import {Injectable} from '@angular/core';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+  constructor() {
+  }
+
+  get(key: string): string {
+    key = key || '';
+
+    return key ? window.localStorage.getItem(key) : '';
+  }
+
+  set(key, value): void {
+    key = key || '';
+    value = value || '';
+
+    if (key) {
+      window.localStorage.setItem(key, value);
+    }
+  }
+
+  remove(key): void {
+    key = key || '';
+
+    if (key) {
+      window.localStorage.removeItem(key);
+    }
+  }
+
+  clear(): void {
+    window.localStorage.clear();
+  }
+}
